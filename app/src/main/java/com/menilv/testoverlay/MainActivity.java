7f0a47f5146eaacaa2de62a11e67e35da0cf036f
MenilV <menil.vukovic@gmail.com>
2018-06-21 14:05:49 +0200

    :bento: Added skip button and styles

--- app/src/main/java/com/menilv/testoverlay/MainActivity.java
+++ app/src/main/java/com/menilv/testoverlay/MainActivity.java
@@ -2,7 +2,6 @@ package com.menilv.testoverlay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,36 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
   @Override protected void onResume() {
     super.onResume();
-
-
-
-    //final CircleOverlayView view  = findViewById(R.id.view);
-    //findViewById(R.id.text).post(new Runnable() {
-    //  @Override public void run() {
-    //    TextView textView = findViewById(R.id.text);
-    //    view.setHighlightView(textView);
-    //
-    //    NewOverlayLayout layout = findViewById(R.id.newlayout);
-    //    layout.setHighlightView(textView);
-    //  }
-    //});
-
     final CustomDialog dialog = new CustomDialog(this, R.style.NewDialog);
     dialog.setCanceledOnTouchOutside(false);
     dialog.setCancelable(false);
     findViewById(R.id.text).post(new Runnable() {
       @Override public void run() {
         dialog.setViewReference(findViewById(R.id.text));
-        //dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.dialog_view);
-
-        Window window = dialog.getWindow();
-        WindowManager.LayoutParams wlp = window.getAttributes();
-
-        //wlp.gravity = Gravity.CENTER;
-        wlp.flags &= ~WindowManager.LayoutParams.FLAG_BLUR_BEHIND;
-        window.setAttributes(wlp);
         dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+        dialog.setSkipButton();
         dialog.show();
       }
     });
